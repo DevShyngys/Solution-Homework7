1f10e1ceeae22e8bcc772e2b0d9a54249005e491
DevShyngys <chingizbulatov72@gmail.com>
2025-04-23 13:36:31 +0500

    Add PassengerPlane class for Mediator Pattern

---
+++ src/mediator/PassengerPlane.java
@@ -0,0 +1,12 @@
+package src.mediator;
+
+public class PassengerPlane extends Aircraft {
+    public PassengerPlane(String id) {
+        super(id);
+    }
+
+    @Override
+    public void receive(String msg) {
+        System.out.println("Passenger Plane " + id + " received: " + msg);
+    }
+}
